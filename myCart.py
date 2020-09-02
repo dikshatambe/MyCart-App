b3f1f2a59dbc2e1705612a42e9f3d9c4e1007cc8
dikshatambe <52398417+dikshatambe@users.noreply.github.com>
2020-09-03 00:56:08 +0530

    Update myCart.py

--- myCart.py
+++ myCart.py
@@ -1,12 +1,12 @@
 from config.config import get_config
 from db.connector.connection import connect_to_db
 from db.models.user import User
-config_path = "/home/sweta/Downloads/MyCart-App/config/configuration.json"
+config_path = "/home/diksha/MyCart-App/config/configuration.json"
 
 if __name__ == "__main__":
     config = get_config(config_path)
     db_conn = connect_to_db(config["db"], config["host"], config["user"],config["password"])
-    user = User(db_conn, id = 1, first_name="Sweta", last_name="Kumari",\
-         email="sweta@chauhan.com",contact_number=1234567890,\
-             address="Jamshedpur",postal_code=831002,password='hello@hello',user_type=1)
+    user = User(db_conn, id = 1, first_name="Saavi", last_name="Sirsat",\
+         email="saavi@chauhan.com",contact_number=1234567890,\
+             address="Pune",postal_code=831002,password='hello@hello',user_type=1)
     user.create_user()
